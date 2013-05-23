b6290d045cc6574957f95bd51105b7651e153580
Vyacheslav Dubeyko <slava@dubeyko.com>
2013-05-23 17:56:32 +0400

    richacl: Add helper function for creating richacl from mode values
    When we don't have richacl stored on disk for permission check
    we need to generate richacl from mode values. This patch adds
    a helper function for doing that.
    
    Signed-off-by: Andreas Gruenbacher <agruen@kernel.org>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    CC: Vyacheslav Dubeyko <slava@dubeyko.com>

--- include/linux/richacl.h
+++ include/linux/richacl.h
@@ -383,5 +383,6 @@ extern struct richacl *richacl_inherit_inode(const struct richacl *,
 
 /* richacl_compat.c */
 extern int richacl_apply_masks(struct richacl **);
+extern struct richacl *richacl_from_mode(mode_t);
 
 #endif /* __RICHACL_H */
